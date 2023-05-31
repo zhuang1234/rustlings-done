94e24c3c63601b55cbcd741c762094c1d20812a0
nashan <z929346631@gmail.com>
2023-05-31 22:48:59 +0800

    update

--- exercises/variables/variables6.rs
+++ exercises/variables/variables6.rs
@@ -1,9 +1,8 @@
 // variables6.rs
 // Execute `rustlings hint variables6` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
-const NUMBER = 3;
+// 常量必须明确显示类型
+const NUMBER: isize = 3;
 fn main() {
     println!("Number {}", NUMBER);
 }
